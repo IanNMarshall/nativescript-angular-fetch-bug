76a84102e6bcae094a18d7176ca8b5a6dae27865
Ian Marshall <ian@onsip.com>
2021-06-30 15:40:42 -0400

    add fetch example

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,24 @@
-import { Component } from '@angular/core'
+import { Component, OnInit } from '@angular/core'
+
+export async function myhttp(
+  request: RequestInfo
+): Promise<any> {
+  const response = await fetch(request);
+  const body = await response.json();
+  return body;
+}
 
 @Component({
   selector: 'ns-app',
   templateUrl: './app.component.html',
 })
-export class AppComponent {}
+export class AppComponent implements OnInit {
+
+  constructor() {}
+
+  ngOnInit() {
+    console.log("MyApp - onInit");
+    const body = myhttp("https://jsonplaceholder.typicode.com/todos");
+    body.then(body => console.log("MyApp - response", body));
+  }
+}
